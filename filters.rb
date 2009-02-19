0ae925c28f035c9eff15b54384e1d730a92dd4a1
Michael Murray <mdmurray@gmail.com>
2009-02-19 11:11:40 -0800

    Merging filters for blog post - http://sudothinker.com/2008/5/1/sf-ruby-meetup-4-29-08

--- filters.rb
+++ filters.rb
@@ -11,3 +11,18 @@ end
 
 p filter_list([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], even?)
 # [2, 4, 6, 8, 10]
+
+def make_filter(predicate)
+  lambda do |list|
+    results = []
+    list.each do |r|
+      results << r if predicate.call(r)
+    end
+    results
+  end
+end
+
+even_filter = make_filter(lambda{|x| x % 2 == 0})
+
+p even_filter.call([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+# [2, 4, 6, 8, 10]
